9dc2dd1e05ce7cc48d44f3c124cc4551357507f7
Phan Anh Tran <kishimotovn@gmail.com>
2024-04-07 07:41:15 +0100

    Initial Commit

---
+++ StarlingTechChallenge/StarlingTechChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarlingTechChallengeApp.swift
+//  StarlingTechChallenge
+//
+//  Created by Phan Anh Tran on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct StarlingTechChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
